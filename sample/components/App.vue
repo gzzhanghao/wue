516fbdd624f92d76b7a03b0aa003816a06d69ba5
jasonhzhnag <jasonhzhang@tencent.com>
2018-01-08 18:57:38 +0800

    update sample

---
+++ sample/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input v-for="a, b in c" v-if="d" @tap.prevent="eventHandler" type="text" />
+    <div>hehe</div>
+    heheehehe{{asdf}}
+    <!-- cmt -->
+  </div>
+</template>
+
+<script>
+  import Loading from './Loading.vue'
+
+  export default {
+
+    components: { Loading },
+  }
+</script>
